fffc1f8c30ae2c40afd9d8b8e61dd1403b73d416
JiangBao <jiangbao1123@gmail.com>
2020-03-18 10:14:06 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>today in history</div>
+    );
+  }
+}
+
+export default App;
